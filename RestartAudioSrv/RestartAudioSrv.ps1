2c396857237775a89241e4125625d1285132ad34
LoneMarvel <tinost@gmail.com>
2022-04-16 12:27:35 +0300

    Restarts Audio & Realtek Audio Service

---
+++ RestartAudioSrv/RestartAudioSrv.ps1
@@ -0,0 +1,12 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+$startOut = [System.Windows.Forms.MessageBox]::Show("Restarting Audio Service","Restart Action Service",1)
+
+if ($startOut -eq 'Cancel'){
+    exit
+}
+else{
+    Restart-Service -Force RtkAudioService
+    Restart-Service -Force Audiosrv
+}
+
+[System.Windows.Forms.MessageBox]::Show("Audio Service Restarted Succesfully","Audio Service Restart",0)
